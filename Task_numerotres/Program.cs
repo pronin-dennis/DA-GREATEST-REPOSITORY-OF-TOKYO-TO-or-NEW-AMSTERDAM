906cac334a33c5ffa02609e0687db85d5369f76d
pronin-dennis <pronin.d.i_ipz24@rcit.ukr.education>
2026-09-23 20:44:34 +0300

    task_three(aka final)
    goto bin/Debug/ .exe

---
+++ Task_numerotres/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_numerotres
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] events = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] last = new int[5];
+
+            for (int i = 0; i < 7; i++)
+            {
+                last[i % 5] = events[i];
+            }
+            Console.WriteLine(last[2]);
+            Console.WriteLine(last[3]);
+            Console.WriteLine(last[4]);
+            Console.WriteLine(last[0]);
+            Console.WriteLine(last[1]);
+        }
+    }
+}
