24f9db9d9b9794f0c8f5c50b8c7823b9e3b709a9
pronin-dennis <pronin.d.i_ipz24@rcit.ukr.education>
2026-09-23 20:39:28 +0300

    task_one
    goto bin/debug/ .exe

---
+++ task_numerouno/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_numerouno
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] log = { 1, 2, 3, 4, 5 };
+
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(log[i]);
+            }
+        }
+    }
+}
