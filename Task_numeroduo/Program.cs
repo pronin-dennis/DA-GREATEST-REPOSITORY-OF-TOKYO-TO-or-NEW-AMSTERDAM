fb4387854b3568670fbeda2ddf3ff2eeb168260b
pronin-dennis <pronin.d.i_ipz24@rcit.ukr.education>
2026-09-23 20:40:32 +0300

    task_two
    goto bin/Debug/ .exe

---
+++ Task_numeroduo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_numeroduo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] month = { 1, 2, 1, 5, 5 };
+            double[] sum = { 100, 200, 50, 300, 150 };
+
+            double[] result = new double[12];
+
+            for (int i = 0; i < 5; i++)
+            {
+                result[month[i] - 1] += sum[i];
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
---
+++ Task_numeroduo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Task_numeroduo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_numeroduo")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9770cdd5-8b90-49d3-a131-bac18c6d5a6e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
